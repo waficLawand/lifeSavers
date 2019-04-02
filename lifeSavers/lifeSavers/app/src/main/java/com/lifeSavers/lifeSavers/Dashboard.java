e8498558e07363bb2deb24af097dcdce31efd65c
Wafic Lawand <wafic.lawand@outlook.com>
2019-04-02 11:43:51 +0300

    init

---
+++ lifeSavers/lifeSavers/app/src/main/java/com/lifeSavers/lifeSavers/Dashboard.java
@@ -0,0 +1,170 @@
+package com.lifeSavers.lifeSavers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Dashboard extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+        Constants constants = new Constants();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+        tx.replace(R.id.flContent, new ServicesFragment());
+        tx.commit();
+
+        setContentView(R.layout.activity_dashboard);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+
+        TextView username = (TextView)headerView.findViewById(R.id.navUsername);
+        TextView email    = (TextView)headerView.findViewById(R.id.navEmail);
+        ImageView userImage = (ImageView)headerView.findViewById(R.id.userImage);
+
+        userImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.flContent,new ProfileFragment()).commit();
+                drawer.closeDrawers();
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        try {
+            JSONObject userData = new JSONObject(getIntent().getStringExtra("userData"));
+            username.setText(userData.getString("username"));
+            email.setText(userData.getString("email"));
+
+
+            Toast.makeText(Dashboard.this, userData.toString(), Toast.LENGTH_SHORT).show();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.dashboard, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Class fragmentClass;
+
+
+        if (id == R.id.balance_fragment) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flContent,new BalanceFragment()).commit();
+
+        } else if (id == R.id.logout) {
+            String URL = "http://"+constants.getIP()+":3000/api/logout";
+            RequestQueue requestQueue = Volley.newRequestQueue(Dashboard.this);
+            StringRequest request = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Toast.makeText(Dashboard.this,"You have successfully logged out!",Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(Dashboard.this,LoginActivity.class);
+                    startActivity(i);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+            requestQueue.add(request);
+            }
+
+
+         else if (id == R.id.home) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flContent,new ServicesFragment()).commit();
+        } /*else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }*/
+
+
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ lifeSavers/lifeSavers/app/src/main/java/com/lifeSavers/lifeSavers/SignupActivity.java
@@ -0,0 +1,239 @@
+package com.lifeSavers.lifeSavers;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "SignupActivity";
+
+    @BindView(R.id.input_name)
+    EditText _nameText;
+    @BindView(R.id.input_address)
+    EditText _addressText;
+    @BindView(R.id.input_email)
+    EditText _emailText;
+    @BindView(R.id.input_mobile)
+    EditText _mobileText;
+    @BindView(R.id.input_password)
+    EditText _passwordText;
+    @BindView(R.id.input_username)
+    EditText _username;
+    @BindView(R.id.input_reEnterPassword)
+    EditText _reEnterPasswordText;
+    @BindView(R.id.btn_signup)
+    Button _signupButton;
+    @BindView(R.id.link_login)
+    TextView _loginLink;
+    @BindView(R.id.bloodType)
+    Spinner bloodTypeSpinner;
+
+    final MapFragment map = new MapFragment();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // Toast.makeText(SignupActivity.this,"jsakjskajskajskajskjakjskaj",Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+        //Initializing map fragment and loadiing it
+        Intent i = getIntent();
+        String location = "";
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction FT = fm.beginTransaction();
+        FT.add(R.id.mapLayout,map);
+        FT.commit();
+        //Disabling address editable text
+        _addressText.setFocusable(false);
+        //Initializing blood type spinner
+        List<String> bloodTypes = new ArrayList<String>();
+
+
+        _signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        if(savedInstanceState ==null)
+        {
+            Toast.makeText(SignupActivity.this,"jsakjskajskajskajskjakjskaj",Toast.LENGTH_SHORT).show();
+
+
+        }
+
+    }
+
+    public void signup() {
+
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        _signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        String name = _nameText.getText().toString();
+        String username = _username.getText().toString();
+        String address = _addressText.getText().toString();
+        String email = _emailText.getText().toString();
+        String mobile = _mobileText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String reEnterPassword = _reEnterPasswordText.getText().toString();
+
+        // TODO: Implement your own signup logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignupSuccess() {
+        _signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(SignupActivity.this,map.getState(),Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        _signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = _nameText.getText().toString();
+        String username = _username.getText().toString();
+        String address = _addressText.getText().toString();
+        String email = _emailText.getText().toString();
+        String mobile = _mobileText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String reEnterPassword = _reEnterPasswordText.getText().toString();
+
+        if (name.isEmpty() || name.length() < 3) {
+            _nameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _nameText.setError(null);
+        }
+        if (username.isEmpty() || name.length() < 3) {
+            _username.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _username.setError(null);
+        }
+        if (address.isEmpty()) {
+            _addressText.setError("Enter Valid Address");
+            valid = false;
+        } else {
+            _addressText.setError(null);
+        }
+
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (mobile.isEmpty() || mobile.length() != 10) {
+            _mobileText.setError("Enter Valid Mobile Number");
+            valid = false;
+        } else {
+            _mobileText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
+            _reEnterPasswordText.setError("Password Do not match");
+            valid = false;
+        } else {
+            _reEnterPasswordText.setError(null);
+        }
+
+        return valid;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("address", _addressText.getText().toString());
+        outState.putString("name", _nameText.getText().toString());
+
+        Toast.makeText(SignupActivity.this,_nameText.getText().toString(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Toast.makeText(SignupActivity.this,"FROM RESTORE!",Toast.LENGTH_SHORT).show();
+    }
+
+
+}
